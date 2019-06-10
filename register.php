7681310c67e1bee3f1b625b6ead4461a7c1a1142
Emika Ijuin <emikaijuin@gmail.com>
2019-06-10 17:03:15 -0400

    pass db connection to account instance

--- register.php
+++ register.php
@@ -1,8 +1,9 @@
 <?php
 include "includes/classes/Account.php";
 include "includes/classes/Constants.php";
+include "includes/config.php";
 
-$account = new Account();
+$account = new Account($con);
 
 include "includes/handlers/login-handler.php";
 include "includes/handlers/register-handler.php";
