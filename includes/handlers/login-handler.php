2236314ab5fa0ccba699914a98951bebe0065c23
Emika Ijuin <emikaijuin@gmail.com>
2019-06-10 17:01:56 -0400

    check login information and log user in if info is valid

--- includes/handlers/login-handler.php
+++ includes/handlers/login-handler.php
@@ -1,5 +1,13 @@
 <?php
-  if(isset($_POST['loginButton'])) {
-    echo "login button was pressed";
-  }
-?>
+if (isset($_POST['loginButton'])) {
+    $username = $_POST['loginUsername'];
+    $password = $_POST['loginPassword'];
+
+    // call login funciton
+    $result = $account->login($username, $password);
+
+    if ($result == true) {
+        $_SESSION['userLoggedIn'] = $username;
+        header("Location: index.php");
+    }
+}
