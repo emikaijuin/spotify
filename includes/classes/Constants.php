ff241a8b44f99ca8c48f9f8584cae35e3fbeb03f
Emika Ijuin <emikaijuin@gmail.com>
2019-06-10 17:01:29 -0400

    store error messages in constants class

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -1,5 +1,6 @@
 <?php
-  class Constants {
+class Constants
+{
     public static $passwordsDoNotMatch = "Your passwords don't match.";
     public static $passwordNotAlphanumeric = "Your password can only contain alphanumeric characters.";
     public static $passwordLength = "Your password must be between 5 and 30 characters.";
@@ -8,5 +9,7 @@
     public static $lastNameLength = "Your last name must be between 5 and 25 characters.";
     public static $firstNameLength = "Your first name must be between 5 and 25 characters.";
     public static $usernameLength = "Your username must be between 5 and 25 characters.";
-  }
-?>
+    public static $usernameTaken = "That username is already taken.";
+    public static $emailTaken = "That email is already taken.";
+    public static $loginFailed = "Your username or password is incorrect.";
+}
