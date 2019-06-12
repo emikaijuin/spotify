abefbfb9f16d090384c22d422d51a78a5d584403
Emika Ijuin <emikaijuin@gmail.com>
2019-06-11 20:46:50 -0400

    create navbar partial

---
+++ includes/navBarContainer.php
@@ -0,0 +1,37 @@
+<div id="navBarContainer">
+  <nav class="navBar">
+    <div class="logo">
+      <a href="index.php">
+        <img src="assets/images/icons/spotify.png" alt="Logo">
+      </a>
+        <h1 class="logoText">
+          <a href="index.php">
+            Spotify
+          </a>
+        </h1>
+    </div>
+
+    <div class="group">
+      <div class="navItem">
+        <a href="search.php" class="navItemLink">Search
+          <img src="assets/images/icons/search.png" class="icon" alt="Search">
+        </a>
+      </div>
+    </div>
+
+    <div class="group">
+      <div class="navItem">
+        <a href="browse.php" class="navItemLink">Browse</a>
+      </div>
+
+      <div class="navItem">
+        <a href="yourMusic.php" class="navItemLink">Your Music</a>
+      </div>
+
+      <div class="navItem">
+        <a href="profile.php" class="navItemLink">Emika Ijuin</a>
+      </div>
+
+    </div>
+  </nav>
+</div>
