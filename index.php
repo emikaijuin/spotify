6e221170ff054fb4ca4326f990c67b0d7ba94540
Emika Ijuin <emikaijuin@gmail.com>
2019-06-11 20:43:27 -0400

    extract html to header and footer partials

--- index.php
+++ index.php
@@ -1,98 +1,24 @@
-<?php
-include "includes/config.php";
+<?php include "includes/header.php";?>
 
-if (isset($_SESSION['userLoggedIn'])) {
-    $userLoggedIn = $_SESSION['userLoggedIn'];
-} else {
-    header("Location: register.php");
+  <h1 class="pageHeadingBig">You Might Also Like</h1>
+
+  <div class="gridViewContainer">
+    <?php
+$albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10;");
+
+while ($row = mysqli_fetch_array($albumQuery)) {
+    echo "<div class='gridViewItem'>
+            <a href='album.php?id="
+        . $row['id'] .
+        "'> <img src='"
+        . $row['artworkPath'] .
+        "' alt='album art'>
+            <div class='gridViewInfo'>"
+        . $row['title'] .
+        "</div>
+            </div>";
 }
 ?>
+  </div>
 
-<!DOCTYPE html>
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="assets/css/styles.css">
-  </head>
-  <body>
-
-    <div id="nowPlayingBarContainer">
-      <div id="nowPlayingBar">
-        <div id="nowPlayingLeft">
-          <div class="content">
-            <span class="albumLink">
-              <img class="albumArtwork" src="assets/images/albums/default.jpg" alt="Album Art">
-            </span>
-
-            <div class="trackInfo">
-              <span class="trackName">
-                <span>Astroworld</span>
-              </span>
-
-              <span class="artistName">
-                <span>Travis Scott</span>
-              </span>
-            </div>
-          </div>
-        </div>
-        <div id="nowPlayingCenter">
-          <div class="content playerControls">
-            <div class="buttons">
-              <button class="controlButton shuffle" title="Shuffle Button">
-                <img src="assets/images/icons/shuffle.png" alt="Shuffle">
-              </button>
-
-              <button class="controlButton previous" title="Previous Button">
-                <img src="assets/images/icons/previous.png" alt="Previous">
-              </button>
-
-              <button class="controlButton play" title="Play Button">
-                <img src="assets/images/icons/play.png" alt="Play">
-              </button>
-
-              <button class="controlButton pause" title="Pause Button" style="display: none">
-                <img src="assets/images/icons/pause.png" alt="Pause">
-              </button>
-
-              <button class="controlButton next" title="Next Button">
-                <img src="assets/images/icons/next.png" alt="Next">
-              </button>
-
-              <button class="controlButton repeat" title="Repeat Button">
-                <img src="assets/images/icons/repeat.png" alt="Repeeat">
-              </button>
-            </div>
-
-            <div class="playbackBar">
-              <span class="progressTime current">0.00</span>
-              <div class="progressBar">
-                <div class="progressBarBg">
-                  <div class="progress"></div>
-                </div>
-              </div>
-              <span class="progressTime remaining">0.00</span>
-            </div>
-          </div>
-        </div>
-        <div id="nowPlayingRight">
-          <div class="volumeBar">
-            <button class="controlButton volume" title="Volume Button">
-              <img src="assets/images/icons/volume.png" alt="Volume">
-            </button>
-
-            <div class="progressBar">
-              <div class="progressBarBg">
-                <div class="progress"></div>
-              </div>
-            </div>
-
-          </div>
-        </div>
-      </div>
-    </div>
-  </body>
-</html>
+<?php include "includes/footer.php";
